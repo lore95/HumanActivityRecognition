c9a188f1ccca89367b58ebde8ac2838902aae91e
llucch <lore95.lucchini@gmail.com>
2025-02-17 17:34:30 +0100

    created code for ml modelling on provided data set

---
+++ Submission/utility_functions.py
@@ -0,0 +1,7 @@
+# OPTIONAL
+# In this file you can include help functions and/or classes that you want to use in your notebook
+# It makes the code more organized and re-usable to have them on a separate .py file
+# Each function and/or class should be separately documented with comments following this pattern:
+# - Use: what does it do/serve
+# - Inputs: input shape and description
+# - Outputs: output shape and description
