1c324f9c4990313c868b361d5cb984d53b19f550
llucch <lore95.lucchini@gmail.com>
2025-02-27 16:15:19 +0100

    final version

--- Submission/utility_functions.py
+++ Submission/utility_functions.py
@@ -1,7 +1,54 @@
-# OPTIONAL
-# In this file you can include help functions and/or classes that you want to use in your notebook
-# It makes the code more organized and re-usable to have them on a separate .py file
-# Each function and/or class should be separately documented with comments following this pattern:
-# - Use: what does it do/serve
-# - Inputs: input shape and description
-# - Outputs: output shape and description
+import pandas as pd
+import matplotlib.pyplot as plt
+import itertools
+
+def plot_activity_counts(df):
+    
+    # Count occurrences of each activity code
+    activity_counts = df['activity'].value_counts().sort_index()
+    
+    # Plotting the bar chart
+    plt.figure(figsize=(10, 6))
+    plt.bar(activity_counts.index, activity_counts.values, color='blue')
+    plt.xlabel('Activity Code')
+    plt.ylabel('Number of Rows')
+    plt.title('Activity Code Frequency')
+    plt.xticks(activity_counts.index)  # Ensure all activity codes are displayed
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    
+    # Display the values on top of the bars
+    for index, value in enumerate(activity_counts.values):
+        plt.text(activity_counts.index[index], value + 0.1, str(value), ha='center', fontsize=12)
+    plt.show()
+
+
+def plot_subject_counts(df):
+   
+    # Count occurrences of each activity code
+    subject_counts = df['subject'].value_counts().sort_index()
+    
+    # Plotting the bar chart
+    plt.figure(figsize=(10, 6))
+    plt.bar(subject_counts.index, subject_counts.values, color='blue')
+    plt.xlabel('Subject Code')
+    plt.ylabel('Number of Rows')
+    plt.title('Subject Code Frequency')
+    plt.xticks(subject_counts.index)  # Ensure all activity codes are displayed
+    plt.grid(axis='y', linestyle='--', alpha=0.7)
+    
+    # Display the values on top of the bars
+    for index, value in enumerate(subject_counts.values):
+        plt.text(subject_counts.index[index], value + 0.1, str(value), ha='center', fontsize=12)
+    plt.show()
+
+
+def group_features_by_prefix(features):
+    feature_groups = {}
+    
+    for feature in features:
+        prefix = "_".join(feature.split("_")[:2])  # Extract first two parts
+        if prefix not in feature_groups:
+            feature_groups[prefix] = []
+        feature_groups[prefix].append(feature)
+    
+    return feature_groups
